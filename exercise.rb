4b20fbb628c2b6a68d3016a608287ca21c216ccd
tenzin2017 <tenzinleo@gmail.com>
2017-08-23 13:56:29 -0400

    finished

---
+++ exercise.rb
@@ -0,0 +1,25 @@
+collections = { data: { rooms:
+    [ { id: 1, room_number: "201", capacity: 50}, { id: 2, room_number: "301", capacity: 200 }, { id: 3, room_number: "1B", capacity: 100}
+    ],
+    events: [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+#Retrieve the capacity of room 201 and store it in a variable.
+
+ capacity_room201 = collections[:data][:rooms][0][:capacity]
+
+ puts capacity_room201
+
+#Find all the events taking place in room 201. Iterate through them and
+#print "ok" if the number of planned attendees will fit in the room.
+
+collections[:data][:events].each do |v|
+  puts v if v[:room_id] == 1
+  puts "OK" if v[:attendees] < capacity_room201
+end
